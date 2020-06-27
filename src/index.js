7f723360867a0d6da839ab5b67c2835c614bc5e5
Jorge <jorgediazok@gmail.com>
2020-06-27 01:29:15 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const config = require('./server/config');
+const app = config(express());
+
+app.listen(app.get('port'), () => {
+  console.log('Server on Port', app.get('port'));
+});
