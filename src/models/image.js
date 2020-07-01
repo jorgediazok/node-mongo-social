0caae44ff2698b817ac5766b7c15ac8479cf6f9e
Jorge <jorgediazok@gmail.com>
2020-07-01 12:36:31 -0300

    schema image done

--- src/models/image.js
+++ src/models/image.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-new Schema({
+const ImageSchema = new Schema({
   title: { type: String },
   description: { type: String },
   filename: { type: String },
+  views: { type: Number, default: 0 },
+  likes: { type: Number, default: 0 },
+  timeStamp: { type: Date, default: Date.now },
 });
+
+module.exports = mongoose.model('Image', ImageSchema);
